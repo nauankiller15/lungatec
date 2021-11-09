b61784c334a8b9f6f176c51efe1161a7593a6f5c
nauankiller15 <nauan.alcantara1@gmail.com>
2021-11-09 12:51:20 -0300

    adicionando robozinho google analytics

--- Includes/Main/head.php
+++ Includes/Main/head.php
@@ -32,4 +32,14 @@
     <!-- ICON -->
     <link rel="icon" type="image/png" href="Imagens/favicon-32x32.png" />
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-211575160-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-211575160-1');
+</script>
 </head>
